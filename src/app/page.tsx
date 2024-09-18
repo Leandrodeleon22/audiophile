505c13f6adea766e092d9ddc2e35fe5855dc1cc5
Leandrodeleon22 <leandrodeleon22@gmail.com>
2024-09-17 20:50:02 -0400

    setup colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="font-bold">MANROPE 123456789!@#$ Q</div>;
+  return <div className="font">MANROPE 123456789!@#$ Q</div>;
 }
