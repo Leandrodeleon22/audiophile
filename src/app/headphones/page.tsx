1192cfa29c316d46747472348fc79196d3d15f64
Leandrodeleon22 <leandrodeleon22@gmail.com>
2024-10-28 20:31:30 -0400

    add headerSimpleComponent

---
+++ src/app/headphones/page.tsx
@@ -0,0 +1,9 @@
+import HeaderSimple from "@/components/HeaderSimple";
+import React from "react";
+
+const page = () => {
+  const title = "Headphones";
+  return <HeaderSimple title={title} />;
+};
+
+export default page;
---
+++ src/components/HeaderSimple.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Wrapper from "./Wrapper";
+
+interface headerSimpleProps {
+  title: string;
+}
+
+const HeaderSimple: React.FC<headerSimpleProps> = ({ title }) => {
+  return (
+    <header className=" min-h-[33.6rem] bg-black  text-white bg flex items-center">
+      <Wrapper>
+        <div className=" flex justify-center">
+          <h1 className="uppercase font-bold text-[4.4rem] tracking-[1.43px] leading-[44px] mt-[7rem]">
+            {title}
+          </h1>
+        </div>
+      </Wrapper>
+    </header>
+  );
+};
+
+export default HeaderSimple;
